750a536c7d9abcc2790e93ed82410c3657d5f2c4
npatilsen <t-npatilsen@microsoft.com>
2021-08-04 13:07:52 -0600

    more tests

--- DigitalTwinsQueryBuilder Smoke Testing/FluentSmokeTesting.cs
+++ DigitalTwinsQueryBuilder Smoke Testing/FluentSmokeTesting.cs
@@ -17,7 +17,7 @@ namespace DigitalTwinsQueryBuilder_Smoke_Testing
                     .IsOfModel("dtmi:example:room;1", true))
                 .Build()
                 .GetQueryText();
-            Debug.Assert(query1 == "SELECT * FROM DigitalTwins WHERE Temperature = 30 AND IS_OF_MODEL('dimi:example:room;1' exact)");
+            Debug.Assert(query1 == "SELECT * FROM DigitalTwins WHERE Temperature = 30 AND IS_OF_MODEL('dimi:example:room;1')");
 
             string query2 = new DigitalTwinsQueryBuilder()
                 .Select("People")
@@ -26,6 +26,24 @@ namespace DigitalTwinsQueryBuilder_Smoke_Testing
                 .Build()
                 .GetQueryText();
             Debug.Assert(query2 == "SELECT People, IsOccupied AS IsOcc FROM Relationships");
+
+            string query3 = new DigitalTwinsQueryBuilder()
+                .Select("Temperature")
+                .Select("Humidity")
+                .Select("IsOccupied", "Occupants")
+                .SelectAs("Owner", "Boss")
+                .From(DigitalTwinsCollection.DigitalTwins)
+                .Build()
+                .GetQueryText();
+            Debug.Assert(query3 == "SELECT Temperature, Humidity, IsOccupied, Occupants, Owner AS Boss FROM DigitalTwins");
+
+            string query4 = new DigitalTwinsQueryBuilder()
+                .Select("T.Temperature", "T.Occupants")
+                .SelectAs("T.People", "Peeps")
+                .From(DigitalTwinsCollection.DigitalTwins, "T")
+                .Build()
+                .GetQueryText();
+            Debug.Assert(query4 == "SELECT T.Temperature, T.Occupants, T.People AS Peeps FROM DigitalTwins T");
         }
     }
 }
